3889a9c08613a7828d729a615bff25721c3b90f8
Francisco <bernardez.f@gmail.com>
2022-08-27 16:55:37 -0700

    initial commit

---
+++ Taller de Syscalls-20220827/src/hai.c
@@ -0,0 +1,13 @@
+#include <signal.h> /* constantes como SIGINT*/
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/reg.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+
+int main(int argc, char* argv[]) {
+  	// Completar
+	return 0;
+}
