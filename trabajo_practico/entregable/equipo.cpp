3d5c55129381263b2b30848cfe2514a33e147e6b
jbarcossc <juancruzbarcos@hotmail.com>
2022-11-03 21:01:05 -0300

    Update equipo.cpp

--- trabajo_practico/entregable/equipo.cpp
+++ trabajo_practico/entregable/equipo.cpp
@@ -45,6 +45,80 @@ void Equipo::jugador(int nro_jugador) {
                 //
                 // ...
                 //
+                /*
+<<<<<<< Updated upstream
+=======*/
+                // Mutex
+
+                // Aseguramos una dirección para que se pueda mover
+                direccion dir_a_mover = this->apuntar_a(this->posiciones[nro_jugador], this->pos_bandera_contraria);
+                bool pudoMover = false;
+                int i = 0;
+                while(i < 4 && pudoMover){
+                    // newPos seria la posicion a la que se moveria si hiciera el movimiento en dir_a_mover
+                    dir_a_mover = direccion((dir_a_mover + i) % 4);
+                    coordenadas newPos = make_pair(this->posiciones[i].first - dir_a_mover == ARRIBA + dir_a_mover == ABAJO,
+                                                   this->posiciones[i].second - dir_a_mover == IZQUIERDA + dir_a_mover == DERECHA);
+                    if(this->belcebu->es_posicion_valida(newPos)){
+                        pudoMover = true;
+                    }
+                }
+
+                if(i < 4){
+                    direccion dir = direccion(dir_a_mover);
+                    this->belcebu->mover_jugador(dir, nro_jugador);
+                }
+
+                // End of mutex
+
+
+                //Moverse
+                /*nro_intento = -1;
+                nro_ronda = -1;
+                //Seccion donde solo entran los jugadores si es el turno de su equipo
+                while(nro_ronda == -1){
+                    nro_intento ++;
+                    //TODO: una funcion mas inteligente para decidir dir_a_mover
+                    direccion dir_a_mover;
+                    if(equipo == ROJO){
+                        dir_a_mover = (nro_intento == 0) ? DERECHA : ARRIBA;
+                    }
+                    else{
+                        dir_a_mover = (nro_intento == 0) ? IZQUIERDA : ABAJO;
+                    }
+                    //nro_ronda devuelve -1 si el lugar al que se quizo mover estaba bloqueado
+
+                }*/
+
+                //Nos aseguramos que todos los jugadores del turno actual esten para comenzar a moverse
+                equipo_coordinacion_mutex.lock();
+                cant_jugadores_listos_para_jugar++;
+                if (cant_jugadores_listos_para_jugar == cant_jugadores){
+                    cant_jugadores_listos_para_jugar = 0;
+                    for (int i = 0; i < cant_jugadores; ++i) {
+                        sem_post(&equipo_coordinacion_sem_entrada);
+                    }
+                }
+                equipo_coordinacion_mutex.unlock();
+                sem_wait(&equipo_coordinacion_sem_entrada);
+
+                nro_ronda = belcebu->mover_jugador(DERECHA, nro_jugador);
+
+                //Si todos movieron, terminamos turno
+                equipo_coordinacion_mutex.lock();
+                cant_jugadores_que_ya_jugaron++;
+                if (cant_jugadores_que_ya_jugaron == cant_jugadores){
+                    //belcebu le va a dar permisos al proximo equipo;
+                    cant_jugadores_que_ya_jugaron = 0;
+                    for (int i = 0; i < cant_jugadores; ++i) {
+                        sem_post(&equipo_coordinacion_sem_salida);
+                    }
+                    belcebu->termino_ronda(equipo);
+                }
+                equipo_coordinacion_mutex.unlock();
+                sem_wait(&equipo_coordinacion_sem_salida);
+
+>>>>>>> Stashed changes
                 break;
 
             case (RR):
